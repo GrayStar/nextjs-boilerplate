cea58eacb537df168fc964b1b3545802ae79d6c8
Adam Grayson <applead@aim.com>
2019-07-02 11:33:16 -0400

    remove the need for express

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-const express = require('express');
+const { createServer } = require('http');
 const next = require('next');
 const routes = require('./routes');
 
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    const server = express();
-    server.use(handler);
-
-    server.get('*', (req, res) => {
-        return handle(req, res);
-    });
-
-    server.listen(3000, (error) => {
+    createServer(handler).listen(port, (error) => {
         if (error) throw error;
-        console.log('App ready on http://localhost:3000');
+        console.log(`App ready on http://localhost:${port}`);
     });
-}).catch((error) => {
-    console.error(error.stack);
-    process.exit(1);
 });
